b704725251e295d9b980809538a447fa46d9ecfd
genku <55231188+genkuqq@users.noreply.github.com>
2026-02-12 01:17:36 +0300

    sqlite update (#4)

---
+++ src/main/java/com/nukku/datamanagers/sqlite/SqliteDatabase.java
@@ -0,0 +1,34 @@
+package com.nukku.datamanagers.sqlite;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public final class SqliteDatabase {
+
+    private static HikariDataSource dataSource;
+
+    public SqliteDatabase(String jdbcUrl) {
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(jdbcUrl);
+        config.setPoolName("SQLiteConnectionPool");
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(3000);
+
+        config.setDriverClassName("org.sqlite.JDBC");
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void shutdown() {
+        dataSource.close();
+    }
+}
